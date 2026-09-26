1a8950d38060767f58c68042c68a465aba5412a7
anfasahammed <anfasahammedpp@gmail.com>
2026-09-26 13:37:26 +0530

    old m2

--- m2.py
+++ m2.py
@@ -1,14 +1,3 @@
-def is_armstrong(n):
-    digits = str(n)
-    power = len(digits)
-    return n == sum(int(d) ** power for d in digits)
-
-
-if __name__ == "__main__":
-    num = int(input("Enter a number: "))
-    if is_armstrong(num):
-        print(f"{num} is an Armstrong number")
-    else:
-        print(f"{num} is not an Armstrong number")
-
-    print("Armstrong numbers up to 1000:", [i for i in range(1, 1001) if is_armstrong(i)])
+sijnrejivneve]
+dfdvvv
+vfdvd
